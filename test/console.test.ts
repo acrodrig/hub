250a3dc2f08eef40054013afb294c80570f727f5
Andres C. Rodriguez <acrodrig@users.noreply.github.com>
2025-04-17 17:42:31 -0700

    Adding test

--- test/console.test.ts
+++ test/console.test.ts
@@ -1,13 +1,23 @@
 #!/usr/bin/env -S deno test -A
 
-import { console } from "../mod.ts#test";
+import { hub } from "../src/hub.ts";
 
 // Not a test perse, but just a test to see if the console replacement works
+const console = hub("test", "debug");
 
 Deno.test("Replacement Console", () => {
   console.debug("debug");
   console.info("info");
   console.warn("warn");
   console.error("error");
+  console.log("log");
+});
+
+Deno.test("Global Console", () => {
+  const log = hub("test", "debug");
+  log.debug("debug");
+  log.info("info");
+  log.warn("warn");
+  log.error("error");
   console.log("log");
 });
