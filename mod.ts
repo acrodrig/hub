0ba61e28c5a3d5fa511d2504b5b5b3f7e9c6cff7
Andres C. Rodriguez <acrodrig@users.noreply.github.com>
2025-04-17 17:15:57 -0700

    Fixing type of returned console to include level

--- mod.ts
+++ mod.ts
@@ -5,7 +5,7 @@ import { CONSOLE, hub, parameters } from "./src/hub.ts";
 // Automatically setup the console replacement and export it? We do this if given a hash on import
 const p: number = import.meta.url?.lastIndexOf("#");
 const ns: string = p >= 0 ? import.meta.url?.substring(p + 1) : "";
-const consoleReplacement: Console = ns ? hub(ns) : CONSOLE;
+const consoleReplacement: Console & { level?: string } = ns ? hub(ns) : CONSOLE;
 
 // Replace console.log to print file and if env variable HUB is set
 const log = console.log;
