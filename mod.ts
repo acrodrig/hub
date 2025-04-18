ad62eb4f10c612e5aaace28bebbaf5d7737c155e
Andres C. Rodriguez <acrodrig@users.noreply.github.com>
2025-04-17 17:12:41 -0700

    Fixing linting

--- mod.ts
+++ mod.ts
@@ -3,9 +3,9 @@ export { CONSOLE, hub, LEVELS, setup } from "./src/hub.ts";
 import { CONSOLE, hub, parameters } from "./src/hub.ts";
 
 // Automatically setup the console replacement and export it? We do this if given a hash on import
-const p = import.meta.url?.lastIndexOf("#");
-const ns = p >= 0 ? import.meta.url?.substring(p + 1) : undefined;
-const consoleReplacement = ns ? hub(ns) : CONSOLE;
+const p: number = import.meta.url?.lastIndexOf("#");
+const ns: string = p >= 0 ? import.meta.url?.substring(p + 1) : "";
+const consoleReplacement: Console = ns ? hub(ns) : CONSOLE;
 
 // Replace console.log to print file and if env variable HUB is set
 const log = console.log;
